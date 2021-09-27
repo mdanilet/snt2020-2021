5e0e78fd10c38d0ad88e5563c50bc2b0965c85d1
mdanilet <maryline.danilet@sjb-lamennais.net>
2021-09-27 12:01:05 +0200

    Create jeu_video_initiation_python.py
    ajout initiation python

---
+++ 2Maths/Python/jeu_video_initiation_python.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Vous pouvez utiliser cette version de Python en ligne :
+# - Tapez votre code ci-dessous ;
+# - pour l'exécuter, regardez au-dessus de ce texte il y a un bouton **Exécuter** ou **Run** avec une flèche ;
+# - pour le rendre dans le cahier de textes : Menu **File** puis **Download As** et enfin **Python** puis le déposer sur le cahier de textes.
+
+# In[ ]:
+
+
+
+
